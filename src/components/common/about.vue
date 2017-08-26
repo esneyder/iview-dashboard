884fa8b22a4b76820e52f25583b8138fbcfee755
itamaradin <itamaradin15@gmail.com>
2017-08-26 17:54:17 -0400

    update about file

--- src/components/common/about.vue
+++ src/components/common/about.vue
@@ -1,49 +1,22 @@
 <style scoped>
-
 .about {
   margin: 10px 20px 0 30px;
   color: black;
 }
-
-</style>
-
-<template>
-
-<div class="about">
-    <vue-markdown># Iview Dashboard</vue-markdown>
-    <vue-markdown>-------------------</vue-markdown>
-    <vue-markdown>### Basado en Vue2 + iView2.0 </vue-markdown>
-    <br/><br/>
-    <vue-markdown># Léame</vue-markdown>
-    <vue-markdown>-------------------</vue-markdown>
-    <vue-markdown>### IView es un marco de interfaz de usuario Vue2 altamente eficiente y hermoso. Principalmente adecuado para los grandes y medianos productos de gama media, tales como una plataforma de funcionamiento del producto, plataforma de monitoreo de datos, plataforma de gestión, desde la configuración del proyecto, a la disposición del diseño, e incluso detrás de la suite de negocios de planificación, es un conjunto de soluciones.
-    </vue-markdown>
-    <vue-markdown>### Utilizado anteriormente Element UI (Vue2) e iView1.0, sentir profundamente la conveniencia de una interfaz de usuario, para mí, personalmente, lo más importante es que puede transferir la energía principal a la información frontal y la lógica.</vue-markdown>
-    <vue-markdown>### Recientemente iView lanzó la versión 2.0 para Vue2, por lo que completaba el ejemplo de un simple sistema de gestión de fondo para fines de aprendizaje.</vue-markdown>
-    <br/><br/>
-    <vue-markdown># La implementación actual de la función y el uso de componentes</vue-markdown>
-    <vue-markdown>-------------------</vue-markdown>
-    <vue-markdown>### Web： [iView2.0](https://github.com/iview/iview)</vue-markdown>
-    <vue-markdown>### Markdown Mostrar： [VueMarkdown](https://github.com/miaolz123/vue-markdown)</vue-markdown>
-    <vue-markdown>### Editor de Markdown： [MarkdownEditor](https://github.com/alecgorge/MarkdownEditor)&amp[Vue-SimpleMDE](https://github.com/F-loat/vue-simplemde)</vue-markdown>
-    <vue-markdown>### Cuadro de texto enriquecido： [quillEditor](https://github.com/surmon-china/vue-quill-editor)</vue-markdown>
-    <vue-markdown>### Gráfico: [Baidu Echarts](http://echarts.baidu.com)&amp[Vue-Echarts](https://github.com/xlsdg/vue-echarts-v3)</vue-markdown>
-    <vue-markdown>###Archivo de carga: [uso iView propio archivo subir componente](https://www.iviewui.com/components/upload)</vue-markdown>
-    <br/><br/>
-    <vue-markdown># Otro</vue-markdown>
-    <vue-markdown>-------------------</vue-markdown>
-    <vue-markdown>### En ausencia de circunstancias especiales, el proyecto continuará manteniendo</vue-markdown>
-</div>
-
-</template>
-
-<script>
-
-import VueMarkdown from 'vue-markdown'
-export default {
-    components: {
-        VueMarkdown
-    }
+h1{
+  margin-top:10%;
+  font-size: 100px;
+  color: #19be6b;
+  text-align: center;
 }
+</style>
+<template>
+<div class="about">
+    <h1>Iview-Dashboard</h1>
+</div>
+</template>
+<script>
+export default {
 
+}
 </script>
